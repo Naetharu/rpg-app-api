5f47323fc15c0f6a4226092b1216327511e1cd52
naetharu <naetharu@gmail.com>
2023-02-18 15:42:08 +0000

    First commit

---
+++ server.ts
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
